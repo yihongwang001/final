ea518f5165944b58bce4aec3d0e7e38a86c6fb7f
Yihong <71856144+yihongwang001@users.noreply.github.com>
2020-12-20 13:31:30 -0800

    heroku

--- auth/authConfig.js
+++ auth/authConfig.js
@@ -16,7 +16,7 @@ module.exports = function configurePassport(app) {
 
 			try {
 				const user = await myDB.findByUsername(username);
-				// Didn't find the user
+				
 				if (!user) {
 					console.log("User not found");
 					return cb(null, false);
@@ -39,13 +39,6 @@ module.exports = function configurePassport(app) {
 		})
 	);
 
-	// Configure Passport authenticated session persistence.
-	//
-	// In order to restore authentication state across HTTP requests, Passport needs
-	// to serialize users into and deserialize users out of the session.  The
-	// typical implementation of this is as simple as supplying the user ID when
-	// serializing, and querying the user record by ID from the database when
-	// deserializing.
 	passport.serializeUser(function (user, cb) {
 		console.log("serializig...", user);
 		cb(null, user.username);
@@ -74,9 +67,6 @@ module.exports = function configurePassport(app) {
 		})
 	);
 
-	// Initialize Passport and restore authentication state, if any, from the
-	// session.
-	// passport.session() acts as a middleware to alter the req object and change the 'user' value that is currently the session id (from the client cookie) into the true deserialized user object.
 	app.use(passport.initialize());
 	app.use(passport.session());
 };
